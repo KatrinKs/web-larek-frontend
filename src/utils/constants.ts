fe8719d44f54c9b3a4b65eff93e3564619c79158
KatrinK <65845086+KatrinKs@users.noreply.github.com>
2025-08-23 02:27:39 +0700

    Update constants.ts

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,17 @@
-export const API_URL = `${process.env.API_ORIGIN}/api/weblarek`;
-export const CDN_URL = `${process.env.API_ORIGIN}/content/weblarek`;
+// export const API_URL = `${process.env.API_ORIGIN}/api/weblarek`;
+// export const CDN_URL = `${process.env.API_ORIGIN}/content/weblarek`;
+const API_ORIGIN = process.env.API_ORIGIN || 'http://localhost:8080';
+
+export const API_URL = `${API_ORIGIN}/api/weblarek`;
+export const CDN_URL = `${API_ORIGIN}/content/weblarek`;
 
 export const settings = {
-
+    currency: 'синапсов',
+    categoryClasses: {
+        'софт-скил': 'card__category_soft',
+        'другое': 'card__category_other',
+        'дополнительное': 'card__category_additional',
+        'кнопка': 'card__category_button',
+        'хард-скил': 'card__category_hard',
+    }
 };
